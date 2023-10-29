6f11b5c6cc9b5375e37a49280ab130f89a8ea430
Corinne <CorinneKogut@gmail.com>
2023-10-29 14:47:14 -0400

    Added number utilities

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -1,36 +1,77 @@
 package io.zipcoder.microlabs.mastering_loops;
 
-
+// Test for this method is testing for odd numbers?
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String even = "";
+
+        for (int i = start; i < stop; i++) {
+            if (i % 2 == 0) {
+                even = even + String.valueOf(i);
+            }
+        }
+        return even;
     }
 
-
+    // Test for this method is testing for even numbers?
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String odd = "";
+
+        for (int i = start; i < stop; i++) {
+            if (i % 2 == 1) {
+                odd = odd + String.valueOf(i);
+            }
+        }
+        return odd;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String squares = "";
+
+        for (int i = start; i < stop; i = i+step) {
+                int squared = i*i;
+                squares = squares + String.valueOf(squared);
+        }
+        return squares;
     }
 
     public static String getRange(int start) {
-        return null;
+        String range = "";
+
+        for (int i = 0; i < start; i++) {
+            range = range + String.valueOf(i);
+        }
+        return range;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String range = "";
+
+        for (int i = start; i < stop; i++) {
+            range = range + String.valueOf(i);
+        }
+        return range;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String range = "";
+
+        for (int i = start; i < stop; i = i + step) {
+            range = range + String.valueOf(i);
+        }
+        return range;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String exponentiate = "";
+
+        for (int i = start; i < stop; i = i+step) {
+            int result = (int) Math.pow(i, exponent);
+            exponentiate = exponentiate + String.valueOf(result);
+        }
+        return exponentiate;
     }
 }
