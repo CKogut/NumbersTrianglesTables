369db8ce1f473f1b54e35e883bb4084bb2209b58
Corinne <CorinneKogut@gmail.com>
2023-10-30 07:55:57 -0400

    Working on TableUtilities, learned about string builder

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,9 +2,23 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
-    }
+        StringBuilder tbl = new StringBuilder();
 
+        //Outer loop. AKA start a row
+        for(int i = 1; i <= 5; i++){
+
+            //Inner loop. AKA fill in the row
+            for(int j = 1; j <=5; j++) {
+                if (i*j < 10) {
+                    tbl = tbl.append("  ").append(i*j).append(" |");
+                } else {
+                    tbl = tbl.append(" ").append(i*j).append(" |");
+                }
+            }
+                tbl = tbl.append("\n");
+        }
+        return tbl.toString();
+    }
     public static String getLargeMultiplicationTable() {
         return null;
     }
