ef2df0527e9d0154a1ab5e89d4e71e0246efc52b
Corinne <CorinneKogut@gmail.com>
2023-10-30 17:53:29 -0400

    Changing to make sense with today's lecture

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,52 +2,20 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        StringBuilder tbl = new StringBuilder();
-
-        //Outer loop. AKA start a row
-        for(int i = 1; i <= 5; i++){
-
-            //Inner loop. AKA fill in the row
-            for(int j = 1; j <=5; j++) {
-                if (i*j < 10) {
-                    tbl = tbl.append("  ").append(i*j).append(" |");
-                } else {
-                    tbl = tbl.append(" ").append(i*j).append(" |");
-                }
-            }
-                tbl = tbl.append("\n");
-        }
-        return tbl.toString();
+        return getMultiplicationTable(5);
     }
     public static String getLargeMultiplicationTable() {
-        StringBuilder tbl = new StringBuilder();
-
-        //Outer loop. AKA start a row
-        for(int i = 1; i <= 10; i++){
-
-            //Inner loop. AKA fill in the row
-            for(int j = 1; j <=10; j++) {
-                if (i*j < 10) {
-                    tbl = tbl.append("  ").append(i*j).append(" |");
-                } else if (i*j < 100){
-                    tbl = tbl.append(" ").append(i*j).append(" |");
-                } else {
-                    tbl = tbl.append("").append(i*j).append(" |");
-                }
-            }
-            tbl = tbl.append("\n");
-        }
-        return tbl.toString();
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
         StringBuilder tbl = new StringBuilder();
 
         //Outer loop. AKA start a row
-        for(int i = 1; i <= 20; i++){
+        for(int i = 1; i <= tableSize; i++){
 
             //Inner loop. AKA fill in the row
-            for(int j = 1; j <=20; j++) {
+            for(int j = 1; j <= tableSize; j++) {
                 if (i*j < 10) {
                     tbl = tbl.append("  ").append(i*j).append(" |");
                 } else if (i*j < 100){
