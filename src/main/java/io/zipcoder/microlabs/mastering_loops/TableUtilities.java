49ffb472d079bc4b0427d1556c8fedb9ce750179
Corinne <CorinneKogut@gmail.com>
2023-10-30 08:02:41 -0400

    Finished TableUtilities

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -20,10 +20,45 @@ public class TableUtilities {
         return tbl.toString();
     }
     public static String getLargeMultiplicationTable() {
-        return null;
+        StringBuilder tbl = new StringBuilder();
+
+        //Outer loop. AKA start a row
+        for(int i = 1; i <= 10; i++){
+
+            //Inner loop. AKA fill in the row
+            for(int j = 1; j <=10; j++) {
+                if (i*j < 10) {
+                    tbl = tbl.append("  ").append(i*j).append(" |");
+                } else if (i*j < 100){
+                    tbl = tbl.append(" ").append(i*j).append(" |");
+                } else {
+                    tbl = tbl.append("").append(i*j).append(" |");
+                }
+            }
+            tbl = tbl.append("\n");
+        }
+        return tbl.toString();
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder tbl = new StringBuilder();
+
+        //Outer loop. AKA start a row
+        for(int i = 1; i <= 20; i++){
+
+            //Inner loop. AKA fill in the row
+            for(int j = 1; j <=20; j++) {
+                if (i*j < 10) {
+                    tbl = tbl.append("  ").append(i*j).append(" |");
+                } else if (i*j < 100){
+                    tbl = tbl.append(" ").append(i*j).append(" |");
+                } else {
+                    tbl = tbl.append("").append(i*j).append(" |");
+                }
+            }
+            tbl = tbl.append("\n");
+        }
+        return tbl.toString();
     }
+
 }
